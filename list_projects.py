689245d049bf5eafea6f220fa6edced06ec676fc
mayuri <mayuritambe777@gmail.com>
2026-01-19 20:39:31 +0530

    listed tasks in JIRA

--- list_projects.py
+++ list_projects.py
@@ -1,13 +1,12 @@
-# This code sample uses the 'requests' library:
-# http://docs.python-requests.org
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://mayuritambe777jira.atlassian.net/rest/api/3/project"
+url = ""
 
-API_TOKEN="ATATT3xFfGF0C3A6_wSW-pa4NLZvNDOGz6Jrgo2blgDMUxuWcKiyaC1D2zBno2y74a0JOsnSH8H18vXMqXG5MpXCqmNVImqKGJNU-v0sola-iLSjwGkAOb3dz8sdBxRbGDpitbcF2IYeqmUpOMtJcZmwHKOjsEnh0sc453JsaotYh7oKGSpBDqQ=83F8DB33"
-auth = HTTPBasicAuth("mayuritambe777@gmail.com", API_TOKEN)
+API_TOKEN=""
+
+auth = HTTPBasicAuth("", API_TOKEN)
 
 headers = {
   "Accept": "application/json"
